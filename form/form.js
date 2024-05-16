a42a69e38287ada32663fe6973ca76ddeee4ada9
Pedro Souto Duarte Junior <36248800+Pedro-Duarte@users.noreply.github.com>
2024-05-16 01:02:59 -0300

    update Index

--- form/form.js
+++ form/form.js
@@ -21,4 +21,29 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('name').value = '';
         document.getElementById('cpf').value = '';
     });
+
+    // Adicionar um evento de "submit" ao formulário
+    meuFormulario.addEventListener('submit', function(evento) {
+        // Verificar qual opção foi selecionada
+        const pacote = document.querySelector('input[name="pacote"]:checked');
+
+        // Verificar se uma opção foi selecionada
+        if (pacote) {
+            // Redirecionar com base no pacote
+            if (pacote.value === "prata") {
+                window.location.href = "../contrato-prata/contrato-prata.html";
+            } else if (pacote.value === "ouro") {
+                window.location.href = "../contrato-ouro/contrato-ouro.html";
+            } else if (pacote.value === "diamante") {
+                window.location.href = "../contrato-diamante/contrato-diamante.html";
+            }
+
+            // Impedir o envio padrão do formulário
+            evento.preventDefault(); // Evita o envio do formulário após o redirecionamento
+        } else {
+            // Caso nenhuma opção seja selecionada, exibir mensagem de erro
+            alert("Por favor, selecione uma opção.");
+            evento.preventDefault(); // Evita o envio do formulário sem opção selecionada
+        }
+    });
 });
